0452c9aa068dec1ecd7ad29bbfafc25820628f68
Aresema Workneh <aworkneh@highrezconsulting.com>
2025-05-12 11:46:16 -0400

    adding the base files

---
+++ server/models/Todo.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  colorTag: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
